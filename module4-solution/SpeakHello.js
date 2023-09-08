dc2b71b53d02fe3f49301a273a0e633ffa2fac81
Haidynada <58096408+haidynada@users.noreply.github.com>
2023-09-08 10:22:26 -0400

    Add files via upload

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,9 @@
+//second Script SpeakHello
+(function (window) {
+  var speakWord = "Hello";
+
+  var helloSpeaker = function speak(name) {
+    console.log(speakWord + " " + name);
+  };
+  window.helloSpeaker = helloSpeaker;
+})(window);
